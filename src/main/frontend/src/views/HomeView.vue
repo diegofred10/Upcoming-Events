09ad350b582ee7984771a1b456bf0ee22856ccf3
Ramiro-isla <ramiroisla11@gmail.com>
2023-02-15 13:40:48 +0100

    First commit

---
+++ src/main/frontend/src/views/HomeView.vue
@@ -0,0 +1,6 @@
+<script setup>
+</script>
+<h1>This is an home page</h1>
+<template>
+</template>
+<style scoped></style>
---
+++ src/test/java/com/UpcomingEvents/UpcomingEvents/UpcomingEventsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.UpcomingEvents.UpcomingEvents;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UpcomingEventsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
