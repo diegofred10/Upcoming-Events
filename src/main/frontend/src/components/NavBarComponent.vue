5242ea31a654d6963f8efa9de158ceb2ad73c1bd
diegofred10 <diegoa.gsm@gmail.com>
2023-02-27 12:43:59 +0100

    merge navbar

---
+++ src/main/frontend/src/components/NavBarComponent.vue
@@ -0,0 +1,39 @@
+<script>
+
+
+</script>
+
+<template>
+  <header>
+    <nav class="navbar navbar-expand-md">
+      <div class="container">
+        <a>
+          <img src="../assets/upcomming-events-logo.png" alt="Upcoming Events Logo"/>
+        </a>
+      </div>
+      <button
+        class="navbar-toggler"
+        type="button"
+        data-bs-toggle="collapse"
+        data-bs-target="#opciones"
+      >
+        
+      </button>
+      <div class="container collapse navbar-collapse">
+        <button class="home">
+          Home
+        </button>
+        <button class="myevents">
+          My Events
+        </button>
+        <div class="hi m-4">
+          Hi, Username
+        </div>
+      </div>
+    </nav>
+  </header>
+</template>
+
+<style scoped>
+@import "../assets/css/NavBarComponent.css"
+</style>
