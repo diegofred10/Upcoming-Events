da9b2555fd30d4851ff1f8a41c31799e3378f9b5
Bryan <bryanpjpgi2b@hotmail.com>
2023-02-28 12:13:28 +0100

    Log In component and Form component did it

--- src/main/frontend/src/views/HomeView.vue
+++ src/main/frontend/src/views/HomeView.vue
@@ -1,9 +1,15 @@
 <script setup>
 import SliderComponent from "../components/SliderComponent.vue";
 import EventComponent from "../components/EventComponent.vue";
+import PopUpForm from "../components/PopUpForm.vue";
+import PopUpLogin from "../components/PopUpLogin.vue";
 </script>
 <template>
 <SliderComponent/>
   <EventComponent />
+  <PopUpForm/>
+  <PopUpLogin/>
+  <EventComponent/>
+  <PopUpLogin/>
 </template>
 <style></style>
