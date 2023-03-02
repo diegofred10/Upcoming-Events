d84c4baa8ebaa7ebacaac4a6bf44ade6e6ec6a91
NataliaCalvo <natalia.calvo@gmail.com>
2023-03-02 14:33:31 +0100

    responsive branch

--- src/main/frontend/src/views/HomeView.vue
+++ src/main/frontend/src/views/HomeView.vue
@@ -2,9 +2,11 @@
 import SliderComponent from "../components/SliderComponent.vue";
 import EventComponent from "../components/EventComponent.vue";
 import Header from "../components/Header.vue"
+import HamburguerMenu from "../components/HamburguerMenu.vue";
 </script>
 <template>
   <Header/>
+  <HamburguerMenu />
 <SliderComponent/>
   <EventComponent />
 </template>
