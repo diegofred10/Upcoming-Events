a038325c390d7d5e9e83275b3ae4d187435fdb07
diegofred10 <diegoa.gsm@gmail.com>
2023-03-07 11:53:57 +0100

    test

--- src/test/java/com/UpcomingEvents/UpcomingEvents/models/UserTest.java
+++ src/test/java/com/UpcomingEvents/UpcomingEvents/models/UserTest.java
@@ -2,43 +2,31 @@ package com.UpcomingEvents.UpcomingEvents.models;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import com.UpcomingEvents.UpcomingEvents.controllers.UserController;
+import com.UpcomingEvents.UpcomingEvents.payloads.UserPayload;
 
 
 
 @SpringBootTest
 public class UserTest {
-    User miUser = new User(1L, "Diego", "12345", "admin", null);
-Event miEvent = new Event(1L, "concierto", null, 20, "ojete calor en concierto", "url.jpg", false);
+    @Autowired
+    UserController usercontroller;
+
+    // Event miEvent = new Event(1L, "concierto", null, 20, "ojete calor en concierto", "url.jpg", false);
+    
     @Test
-void UserNameIsDiego(){
-String name = miUser.getName();
+    void UserNameIsDiego(){
+    UserPayload miUser = new UserPayload(2L, "Diego", "12345", "admin");
+    usercontroller.save(miUser);
+    String name = usercontroller.getOne(2L).getName();
 assertEquals("Diego", name);
 }
 
-@Test
-void PasswordIsRight(){
-    String password = miUser.getPassword();
-    assertEquals("12345", password);
-} 
-@Test
-void RolIsAdmin(){
-    String rol = miUser.getRol();
-    assertEquals("admin", rol);
-}
-@Test
-void UserHaveEvent(){
-    List<Event> eventList = new ArrayList<Event>();
-    eventList.add(miEvent);
-    eventList.add(miEvent);
-    miUser.setEvents(eventList);
-    List<Event> resultList = miUser.getEvents();
-    assertEquals("concierto", resultList.get(0).getTitle());
-}
+
+
 
 }
