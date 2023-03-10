bf608ea996968ed38d0c070e0e34b34d818fb909
SaulAguinaga <saul_aguinaga@hotmail.es>
2023-03-10 09:39:14 +0100

    delete event

--- src/test/java/com/UpcomingEvents/UpcomingEvents/models/UserTest.java
+++ src/test/java/com/UpcomingEvents/UpcomingEvents/models/UserTest.java
@@ -2,16 +2,13 @@ package com.UpcomingEvents.UpcomingEvents.models;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import com.UpcomingEvents.UpcomingEvents.controllers.UserController;
 import com.UpcomingEvents.UpcomingEvents.payloads.UserPayload;
-import com.UpcomingEvents.UpcomingEvents.repositories.UserRepository;
+
 
 
 
@@ -33,27 +30,6 @@ void UserNameIsDiego(){
     String name = controller.getOne(2L).getName();
 
 assertEquals("Diego", name);
-}
-
-
-@Test
-void PasswordIsRight(){
-    String password = miUser.getPassword();
-    assertEquals("12345", password);
-} 
-@Test
-void RolIsAdmin(){
-    String rol = miUser.getRol();
-    assertEquals("admin", rol);
-}
-@Test
-void UserHaveEvent(){
-    List<Event> eventList = new ArrayList<Event>();
-    eventList.add(miEvent);
-    eventList.add(miEvent);
-    miUser.setEvents(eventList);
-    List<Event> resultList = miUser.getEvents();
-    assertEquals("concierto", resultList.get(0).getTitle());
 }
 
 }
