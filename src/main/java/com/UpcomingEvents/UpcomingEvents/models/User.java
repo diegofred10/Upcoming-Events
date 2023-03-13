8e98e44d8734d3523bfe0fffea352fff142fe52f
diegofred10 <diegoa.gsm@gmail.com>
2023-03-13 14:54:59 +0100

    change

--- src/main/java/com/UpcomingEvents/UpcomingEvents/models/User.java
+++ src/main/java/com/UpcomingEvents/UpcomingEvents/models/User.java
@@ -5,37 +5,23 @@ import java.util.Set;
 
 import javax.persistence.*;
 
-
-
 @Entity
-@Table(name = "users" )
+@Table(name = "users")
 
 public class User {
-    @Id @GeneratedValue(strategy = GenerationType.IDENTITY) @Column(name= "id_user")
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_user")
     private Long id;
     private String userName;
     private String password;
-    @ManyToMany(fetch = FetchType.EAGER)
-    @JoinTable(name = "roles_users", joinColumns = @JoinColumn(name= "user_id"),inverseJoinColumns = @JoinColumn(name= "role_id"))
-    Set<Role> roles;
-    public Set<Role> getRoles() {
-        return roles;
-    }
-
-
-    public void setRoles(Set<Role> roles) {
-        this.roles = roles;
-    }
-
-
     private String role;
+
     @ManyToMany
     private List<Event> events;
 
-
     public User() {
     }
-
 
     public User(Long id, String userName, String password, String role, List<Event> events) {
         this.id = id;
@@ -44,60 +30,45 @@ public class User {
         this.role = role;
         this.events = events;
     }
-    
 
     public Long getId() {
         return id;
     }
 
-
     public void setId(Long id) {
         this.id = id;
     }
-
 
     public String getUserName() {
         return userName;
     }
 
-
     public void setUserName(String userName) {
         this.userName = userName;
     }
-
 
     public String getPassword() {
         return password;
     }
 
-
     public void setPassword(String password) {
         this.password = password;
     }
-
 
     public String getRole() {
         return role;
     }
 
-
     public void setRole(String role) {
         this.role = role;
     }
-
 
     public List<Event> getEvents() {
         return events;
     }
 
-
     public void setEvents(List<Event> events) {
         this.events = events;
     }
 
-    
-
-    
-
-    
 }
--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -4,3 +4,5 @@ INSERT INTO users VALUES (default, "Johel", "1111", "user");
 INSERT INTO events VALUES (default,1, "2023-02-17", "concierto taburete", "url.jpg", 100, 0, "Taburete");
 INSERT INTO events VALUES (default,1, "2023-06-01", "concierto rolling", "url.jpg", 500, 0, "Rolling");
 INSERT INTO events VALUES (default,1, "2023-05-30", "concierto carol G", "url.jpg", 300, 0, "Carol G");
+
+
